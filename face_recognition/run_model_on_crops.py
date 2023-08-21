c7c69c1613942e729c5ef5143a63ef441bc3b0f5
shakedva <shakediii123@gmail.com>
2023-08-21 14:33:45 +0300

    Added threshold for fatigue detection

--- face_recognition/run_model_on_crops.py
+++ face_recognition/run_model_on_crops.py
@@ -4,42 +4,63 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-OPEN_LABEL: str = 'Open_Eyes'
-CLOSED_LABEL: str = 'Closed_Eyes'
-crop_images = []
-DEFAULT_BASE_DIR: str = 'resources'
-CROPS_PATH = DEFAULT_BASE_DIR + "/crops"
-best_model = load_model('bestModel.h5')
 
+class EyeClassifier:
+    def __init__(self, model_path='bestModel.h5', base_dir='resources'):
+        self.OPEN_LABEL = 'Open_Eyes'
+        self.CLOSED_LABEL = 'Closed_Eyes'
+        self.CROPS_PATH = os.path.join(base_dir, 'crops')
+        self.best_model = load_model(model_path)
+        self.crop_images = []
 
-for filename in os.listdir(CROPS_PATH):
-    if filename.endswith(".jpg"):
-        image_path = os.path.join(CROPS_PATH, filename)
-        img = Image.open(image_path).resize((64, 64)).convert('L')  # Convert to grayscale
+    def preprocess_image(self, img):
+        img = img.resize((64, 64)).convert('L')
         img = np.array(img)
         img = (img - np.min(img)) / (np.max(img) - np.min(img))
         img = img / 255.0
         img = np.expand_dims(img, -1)
-        crop_images.append(img)
+        return img
 
-crop_images = np.array(crop_images)
+    def load_images(self):
+        for filename in os.listdir(self.CROPS_PATH):
+            if filename.endswith(".jpg"):
+                image_path = os.path.join(self.CROPS_PATH, filename)
+                img = Image.open(image_path)
+                img = self.preprocess_image(img)
+                self.crop_images.append(img)
+        self.crop_images = np.array(self.crop_images)
 
-open_count = 0
-close_count = 0
-for img in crop_images:
-    result = best_model.predict(np.expand_dims(img, 0))
-    plt.imshow(img.squeeze(), cmap='gray')
-    plt.axis('off')
-    plt.show()
+    def classify_images(self):
+        open_count = 0
+        close_count = 0
+        for img in self.crop_images:
+            result = self.best_model.predict(np.expand_dims(img, 0))
+            # plt.imshow(img.squeeze(), cmap='gray')
+            # plt.axis('off')
+            # plt.show()
 
-    if result > 0.5:
-        open_count += 1
-        # print(OPEN_LABEL)
-        # plt.text(5, 5, OPEN_LABEL, color='green', fontsize=12, bbox=dict(facecolor='white', alpha=0.8))
-    else:
-        close_count += 1
-        # print(CLOSED_LABEL)
-        # plt.text(5, 5, CLOSED_LABEL, color='red', fontsize=12, bbox=dict(facecolor='white', alpha=0.8))
+            if result > 0.5:
+                open_count += 1
+                # plt.text(5, 5, self.OPEN_LABEL, color='green', fontsize=12, bbox=dict(facecolor='white', alpha=0.8))
+            else:
+                close_count += 1
+                # plt.text(5, 5, self.CLOSED_LABEL, color='red', fontsize=12, bbox=dict(facecolor='white', alpha=0.8))
 
-print(f"{OPEN_LABEL}: {open_count}")
-print(f"{CLOSED_LABEL}: {close_count}")
+        return open_count, close_count
+
+    def run(self):
+        self.load_images()
+        open_count, close_count = self.classify_images()
+        count = open_count + close_count
+        close_percentage = (close_count / count) * 100
+
+        # print(f"{self.OPEN_LABEL}: {open_count}")
+        # print(f"{self.CLOSED_LABEL}: {close_count}")
+
+        if close_percentage >= 60:
+            print("Person is asleep. Turn on alert.")
+
+
+if __name__ == "__main__":
+    eye_classifier = EyeClassifier()
+    eye_classifier.run()
