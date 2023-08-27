38b1d59bf3878dab6ab487cec4476cf18e2a9448
MosheNamdar <110346394+MosheNamdar@users.noreply.github.com>
2023-08-27 15:30:56 +0300

    refactor camera

--- camera.py
+++ camera.py
@@ -1,41 +1,38 @@
 import time
-import picamera
-import keyboard
+import cv2
+
+DEFAULT_BASE_DIR: str = 'resources'
 
 
 class ContinuousPhotoCapture:
     def __init__(self):
-        self.camera = picamera.PiCamera()
-        self.camera.resolution = (1920, 1080)
+        # Initialize the OpenCV video capture object
+        self.camera = cv2.VideoCapture(0)
+        # Set the resolution
+        self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
         self.running = False
 
-    def capture_photos(self, photos_per_second):
-        self.running = True
+    def capture_photos(self, photos_to_capture):
+        # self.running = True
+        # print("Press any key to stop capturing.")
 
-        # Display instructions for stopping the loop
-        print("Press any key to stop capturing.")
+        # try:
+        captured_count = 0  # Counter for captured photos
+        while captured_count < photos_to_capture:
+            ret, frame = self.camera.read()
+            if not ret:
+                # print("Failed to grab frame.")
+                continue
+            filename = time.strftime(DEFAULT_BASE_DIR + f"/photo_{captured_count}_%Y%m%d%H%M%S.jpg")
+            cv2.imwrite(filename, frame)
+            captured_count += 1
 
-        try:
-            while self.running:
-                start_time = time.time()
-
-                for i in range(3):
-                    filename = time.strftime("photo_%Y%m%d%H%M%S.jpg")
-                    self.camera.capture(filename)
-                    print(f"Captured {filename}")
-
-                elapsed_time = time.time() - start_time
-                if elapsed_time < 1:
-                    time.sleep(1 - elapsed_time / photos_per_second)
-
-                if keyboard.is_pressed():
-                    print("Capture stopped by user.")
-                    break
-
-        finally:
-            self.camera.close()
+        print(f"{captured_count} photos captured.")
+        # finally:
+        #     self.camera.release()
 
 
 if __name__ == "__main__":
     capture_instance = ContinuousPhotoCapture()
-    capture_instance.capture_photos(photos_per_second=3)
+    capture_instance.capture_photos(photos_to_capture=3)
